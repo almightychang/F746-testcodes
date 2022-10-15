ca773a7b83285f6375695fbc0ef288351e898545
aniai <joochul@aniai.ai>
2022-10-15 18:10:23 +0900

    ignore build

--- build/pcb.d
+++
@@ -1,4 +0,0 @@
-build/pcb.o: fw-lib-stm32-CocktailOS/core/pcb.c \
- fw-lib-stm32-CocktailOS/core/pcb.h fw-lib-stm32-CocktailOS/core/task.h
-fw-lib-stm32-CocktailOS/core/pcb.h:
-fw-lib-stm32-CocktailOS/core/task.h:
--- build/task.d
+++
@@ -1,3 +0,0 @@
-build/task.o: fw-lib-stm32-CocktailOS/core/task.c \
- fw-lib-stm32-CocktailOS/core/task.h
-fw-lib-stm32-CocktailOS/core/task.h:
